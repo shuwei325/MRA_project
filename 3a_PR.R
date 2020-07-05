d06cd517e81e0b1d3f9e92342838de866b5b4b02
Shu Wei <vicchou325@gmail.com>
2020-07-05 14:10:33 -0600

    5/7/2020

---
+++ 3a_PR.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+library(fracdiff)
+library(ggplot2)
+library(ggcorrplot)
+library(corrplot)
+library(RColorBrewer)
+library(forecast)
+library(fpp2)
+library(ggfortify)
+library(dplR)
+library(wavethresh)
+library(WaveletComp)
+library(astsa)
+
+load(file ="pr_hour.Rdata")
+str(datos)
+time<-as.numeric(row.names(datos))
+
+datos.d <- datos %>% mutate(time.d=floor(time)) %>%
+  group_by(time.d) %>% summarise_each( funs(mean))
+
+names(datos.d)
+dim(datos.d)
+dim(datos)
+attach(datos.d)
+
+datos.ts<-ts(datos.d[,-1])
+
+autoplot(datos.ts)
+autoplot(datos.ts, facets = TRUE)
+
+(corr<-cor(datos.ts))
+#ggcorrplot(corr)
+corrplot(corr, type="upper", order="hclust",
+         col=brewer.pal(n=8, name="RdYlBu"))
+
+datos.ts<-ts(datos)
+y1<-datos.ts[,"y1"]
+y2<-datos.ts[,"y2"]
+y3<-datos.ts[,"y3"]
+y4<-datos.ts[,"y4"]
+y5<-datos.ts[,"y5"]
+y6<-datos.ts[,"y6"]
+y7<-datos.ts[,"y7"]
+y8<-datos.ts[,"y8"]
+y9<-datos.ts[,"y9"]
+
+par(mfrow=c(3,3))
+acf(y1,lag.max=100,ci=0.95)
+acf(y2,lag.max=100,ci=0.95)
+acf(y3,lag.max=100,ci=0.95)
+acf(y4,lag.max=100,ci=0.95)
+acf(y5,lag.max=100,ci=0.95)
+acf(y6,lag.max=100,ci=0.95)
+acf(y7,lag.max=100,ci=0.95)
+acf(y8,lag.max=100,ci=0.95)
+acf(y9,lag.max=100,ci=0.95)
+
+par(mfrow=c(1,3))
+ts.plot(y1)
+acf(y1,lag.max=500,ci=0.95)
+pacf(y1,lag.max=500,ci=0.95)
+
+
+# Spectral Analysis
+par(mfrow=c(1,2))
+raw.spec1 <- spec.pgram(y1, taper = 0)
+plot(raw.spec1, log = "no")
+
+ts.plot(datos.ts, col=1:9)
+spec = mvspec(datos.ts, spans=c(3,3), taper=.1)
+
+my.w1 <- analyze.wavelet(datos.d, "y1",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 500,make.pval = TRUE, n.sim = 10)
+my.w2 <- analyze.wavelet(datos.d, "y2",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 500,make.pval = TRUE, n.sim = 10)
+my.w4 <- analyze.wavelet(datos.d, "y4",loess.span = 0,
+                         dt = 1, dj = 1/250,lowerPeriod = 1,
+                         upperPeriod = 500,make.pval = TRUE, n.sim = 10)
+
+
+wt.image(my.w1, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+wt.image(my.w2, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+wt.image(my.w4, color.key = "quantile", n.levels = 250,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7))
+
+
--- README.md
+++ README.md
@@ -5,25 +5,29 @@
 
 ### Regional variables:
  1. Temperature:
-	* [hourly data](https://shuwei325.github.io/MRA_project/TAS_hour/).
-	* [monthly data](https://shuwei325.github.io/MRA_project/TAS_month/).
+	* [hourly data](https://shuwei325.github.io/MRA_project/2a_TAS_hour.html).
+	* [monthly data](https://shuwei325.github.io/MRA_project/2a_TAS_month.html).
  2. Precipitation:
-	* [hourly data](https://shuwei325.github.io/MRA_project/PR_hour/).
-	* [hourly data](https://shuwei325.github.io/MRA_project/PR_month/).
+	* [hourly data](https://shuwei325.github.io/MRA_project/2a_PR_hour.html).
+	* [hourly data](https://shuwei325.github.io/MRA_project/2a_PR_month.html).
+
+### Regional variables (daily):
+ 1. [Temperature](https://shuwei325.github.io/MRA_project/3a_TAS.html).
+ 2. [Precipitation](https://shuwei325.github.io/MRA_project/3a_PR.html).
 
 ### Global variables (monthly):
 
- 1. [Omega (vertical velocity)](https://shuwei325.github.io/MRA_project/4a_OMEGA_global/) 
+ 1. [Omega (vertical velocity)](https://shuwei325.github.io/MRA_project/4a_OMEGA_global.html) 
 	* units: Pa/s
 
- 2. [U (zonal wind)](https://shuwei325.github.io/MRA_project/4b_U_global/) 
+ 2. [U (zonal wind)](https://shuwei325.github.io/MRA_project/4b_U_global.html) 
 	* units: m/s
 
- 3. [V (meridinal wind)](https://shuwei325.github.io/MRA_project/4c_V_global/) 
+ 3. [V (meridinal wind)](https://shuwei325.github.io/MRA_project/4c_V_global.html) 
 	* units: m/s
 
- 4. [PSL (sea level pressure)](https://shuwei325.github.io/MRA_project/4d_PSL_global/) 
+ 4. [PSL (sea level pressure)](https://shuwei325.github.io/MRA_project/4d_PSL_global.html) 
 	* units: Pa
 
- 5. [TREFHT (reference height temperature)](https://shuwei325.github.io/MRA_project/4e_TREFHT_global/) 
+ 5. [TREFHT (reference height temperature)](https://shuwei325.github.io/MRA_project/4e_TREFHT_global.html) 
 	* units: K
